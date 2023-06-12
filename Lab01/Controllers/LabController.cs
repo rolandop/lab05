2ad28704577c5328ff881f706d198367c4027653
Peña Molina Angel Rolando <penaa@asegsur3.local>
2023-06-12 17:46:46 -0500

    Primera carga

---
+++ Lab01/Controllers/LabController.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab01.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LabController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<LabController> _logger;
+
+        public LabController(ILogger<LabController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [Route("AddHistory")]
+        public string AddHistory()
+        {
+            _logger.LogInformation("AddHistory");
+
+            try
+            {
+                var path = "/var/Lab/logs";
+
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                var file = $"{path}/{DateTime.Now.ToString("ddMMyyyy_HH_mm_ss")}.log";
+
+                System.IO.File.WriteAllText(file, "Log de pruebas");
+
+                return "OK";
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error en AddHistory");
+                return "ERROR";
+            }
+        }
+    }
+}
